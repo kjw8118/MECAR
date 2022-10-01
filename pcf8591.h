e11e047d1b6f0f74e39ca86533c76ca0e13698d3
Jinwon_PI <kjw8118@gmail.com>
2022-10-02 01:11:09 +0900

    Create enum for PIN number

--- pcf8591.h
+++ pcf8591.h
@@ -10,7 +10,14 @@ private:
     const uint8_t addr = 0x48;
     const uint8_t cmd = 0x40;
     Wire i2c;
-public:    
+public:
+    enum PCF8591_PIN
+    {
+        AIN0 = 0,
+        AIN1 = 1,
+        AIN2 = 2,
+        AIN3 = 3,
+    };
     void init();
     int analogRead(uint8_t pin);
     void analogWrite(uint8_t val);    
