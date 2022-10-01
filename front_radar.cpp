14c98f9adadaa53f6b098559f1e3adc6aa79d2d8
Jinwon_PI <kjw8118@gmail.com>
2022-10-02 01:13:20 +0900

    Apply periodic run and generate state transition

--- front_radar.cpp
+++ front_radar.cpp
@@ -2,7 +2,11 @@
 #include "pcf8591.h"
 
 #include <iostream>
-using namespace std;
+
+#include <map>
+#include <cmath>
+
+
 
 double FrontRadar::volt2mm(int volt)
 {
@@ -11,29 +15,174 @@ double FrontRadar::volt2mm(int volt)
     if (volt < 0)
         volt = 0;
 
-    return (24.61 / (volt - 0.1696)) * 10000;
+    return (40 / (double)volt) * 10000;
+}
+double FrontRadar::volt2cm(int volt)
+{    
+
+    return this->volt2mm(volt)/10;
+}
+void FrontRadar::check_object_state()
+{
+    double dist = this->target_distance;
+    int state = this->object_state;
+    if(state == this->NO_OBJECT)
+    {
+        if(dist < this->THRESHOLD_FAR - this->THRESHOLD_HYS)
+        {
+            this->object_state = this->FAR;
+        }
+        
+    }
+    else if(state == this->FAR)
+    {
+        if(dist < this->THRESHOLD_NEAR - this->THRESHOLD_HYS)
+        {
+            this->object_state = this->NEAR;
+        }
+        else if(dist > this->THRESHOLD_FAR + this->THRESHOLD_HYS)
+        {
+            this->object_state = this->NO_OBJECT;
+        }
+    }
+    else if(state == this->NEAR)
+    {
+        if(dist < this->THRESHOLD_INVALID)
+        {
+            this->object_state = this->INVALID;
+        }
+        else if(dist > this->THRESHOLD_NEAR + this->THRESHOLD_HYS)
+        {
+            this->object_state = this->FAR;
+        }
+    }
+    else if(state == this->INVALID)
+    {
+        if(dist > this->THRESHOLD_VALID)
+        {
+            this->object_state = this->NEAR;
+        }
+        
+    }
 }
 
 void FrontRadar::init()
 {
-    this->pin = 3;
+    this->pin = PCF8591::AIN3;
     this->pcf8591.init();
 }
 
-double FrontRadar::getDistance()
+void FrontRadar::getTargetDistance()
 {
-    int volt = (int)((double)this->pcf8591.analogRead(this->pin)*5000/255);
-    double dist = this->volt2mm(volt);
-    return dist * this->k + this->past * (1 - this->k);
+    
+    this->target_distance_past = this->target_distance;
+
+    int volt = (int)((double)this->pcf8591.analogRead(this->pin)*5000/255);    
+    double dist = this->volt2cm(volt);
+
+    //std::cout << "volt: " << volt << " dist: " << dist << std::endl;
+    if(std::abs(dist - this->target_distance_past) < 30)
+        this->target_distance = dist * this->k + this->target_distance * (1 - this->k);
+    else
+        this->target_distance = dist * 1 + this->target_distance * 0;
+    
+    
 }
 
+void FrontRadar::getTargetSpeed()
+{    
+    double dt = this->timer.lead_ms();
+    
+    if(10 != 0)
+    {
+        double dist_diff = this->target_distance - this->target_distance_past;
+        this->target_speed_past = this->target_speed;
+
+        double spd = dist_diff/(100*0.001);
+        //std::cout << "spd: " << spd << " diff " << dist_diff;
+        
+        this->target_speed = spd*this->vk + this->target_speed*(1-this->vk);
+        //std::cout << (double)this->timer.lead_ms() << " " << dist_diff << " " << this->target_speed << std::endl;
+    }
+}
+void FrontRadar::getTTC()
+{
+    double dist = this->target_distance;
+    double spd = this->target_speed;
+    if(spd < 0)
+    {
+        this->target_ttc = -dist/spd;
+    }
+}
+
+void FrontRadar::check_operation_state()
+{
+    int target_state = this->object_state;
+    int op_state = this->operation_state;
+    switch(target_state)
+    {
+        case this->NO_OBJECT:
+            this->operation_state = this->NO_DETECTION;
+            break;
+        case (this->FAR || this->NEAR):            
+            if(this->target_ttc > 10)
+            {
+                this->operation_state = this->DETECTION;
+            }
+            else
+            {
+                if(this->target_ttc > 5)
+                {
+                    this->operation_state = this->COLLISION;
+                }
+                else
+                    this->operation_state = this->WARNING;
+            }            
+            break;
+        case this->INVALID:
+            break;
+
+    }
+
+}
 void FrontRadar::run()
 {
     this->init();
-
+    this->timer.set_t0_ms();
+    unsigned int tick_10ms = 0, tick_100ms = 0, tick_1000ms;
     while(true)
     {
-        cout << "Dist: " << this->getDistance() << endl;
+        //this->timer.wait_until_ms(this->period*1000);
+        //this->timer.set_t0_ms();
+        //this->timer.wait_until_ms(1);
+        unsigned int tick_ms = this->timer.getTick_ms();
+        if(tick_ms % 10 == 0 && tick_10ms != tick_ms)
+        {
+            tick_10ms = tick_ms;
 
+            this->getTargetDistance();
+            this->getTTC();            
+            this->check_object_state();
+            this->check_operation_state();
+        }
+        if(tick_ms % 100 == 0  && tick_100ms != tick_ms)
+        {
+            tick_100ms = tick_ms;
+            
+            
+            this->getTargetSpeed();
+            
+            
+            std::cout << " Tick: " << tick_ms << " Obj: " << this->object_state_map[this->object_state] << " Opr: " << this->operation_state_map[this->operation_state] << " Dist: " << this->target_distance << " Spd: " << this->target_speed << " TTC: " << this->target_ttc << std::endl;
+        }
+        if(tick_ms % 1000 == 0 && tick_1000ms != tick_ms)
+        {
+            tick_1000ms = tick_ms;
+            
+            
+        }
+        
+        
+        //this->timer.set_t0_ms();
     }
 }
--- front_radar.h
+++ front_radar.h
@@ -2,20 +2,65 @@
 #define __FRONTRADAR_H__
 
 #include "pcf8591.h"
+#include "timer.h"
+
+#include <map>
+#include <string>
 
 class FrontRadar
 {
 private:
-    int pin;
-    double past = 0;
-    double k = 0.2;    
+    std::map<int, std::string> operation_state_map = {{0, "NO_DETECTION"}, {1, "DETECTION"}, {2, "WARNING"}, {3, "COLLISION"}};
+    std::map<int, std::string> object_state_map = {{0, "NO_OBJECT"}, {1, "FAR"}, {2, "NEAR"}, {3, "INVALID"}};
+    int pin;    
+    double k = 0.05;
+    double vk = 0.2;
+    double period = 0.1;
     double volt2mm(int volt);
+    double volt2cm(int volt);
     PCF8591 pcf8591;
+    Timer timer;
+    enum CONDITION
+    {
+        NO_OBJECT = 0,
+        FAR = 1,
+        NEAR = 2,
+        INVALID = 3,
+        
+    };
 
+    enum OPERATION
+    {
+        NO_DETECTION = 0,
+        DETECTION = 1,
+        WARNING = 2,
+        COLLISION = 3,        
+    };
+    
+    enum THRESHOLD
+    {
+        THRESHOLD_HYS = 2,
+        THRESHOLD_INVALID = 10,
+        THRESHOLD_VALID = 20,
+        THRESHOLD_NEAR = 30,
+        THRESHOLD_FAR = 60,
+    };
+    int object_state = NO_OBJECT;
+    int operation_state = NO_DETECTION;
+    void check_object_state();
+    void getTargetDistance();
+    void getTargetSpeed();
+    void getTTC();
+    void check_operation_state();
+    double target_distance = THRESHOLD_FAR;
+    double target_distance_past = THRESHOLD_FAR;
+    double target_speed = 0;    
+    double target_speed_past = 0;
+    double target_ttc = THRESHOLD_FAR;
 public:
     FrontRadar(/* args */){};
     void init();
-    double getDistance();
+    
     void run();
     ~FrontRadar(){};
 };
