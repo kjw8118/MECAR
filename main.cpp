794680af2f02e1632b73c34a36c9a1c672462c27
Jinwon_PI <kjw8118@gmail.com>
2022-10-02 01:13:47 +0900

    Test radar thread

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 #include "gpio_interface.h"
 
-#include "powertrain.h"
+//#include "powertrain.h"
 //#include "front_camera.h"
-//#include "front_radar.h"
+#include "front_radar.h"
 //#include "lidar.h"
-#include "speedometer.h"
-#include "joystick.h"
+//#include "speedometer.h"
+//#include "joystick.h"
 
 
 int main()
@@ -17,18 +17,18 @@ int main()
     thread gpio_thread = thread(&GPIO::init_gpio);
     gpio_thread.join();
 
-    thread pt_thread = thread(&Powertrain::run, Powertrain());
+    //thread pt_thread = thread(&Powertrain::run, Powertrain());
     //thread cam_thread = thread(&FrontCamera::run, FrontCamera());
-    //thread radar_thread = thread(&FrontRadar::run, FrontRadar());
+    thread radar_thread = thread(&FrontRadar::run, FrontRadar());
     //thread lidar_thread = thread(&LiDAR::run, LiDAR());
-    thread speedometer_thread = thread(&Speedometer::run, Speedometer());
-    thread joystick_thread = thread(&Joystick::run, Joystick());
+    //thread speedometer_thread = thread(&Speedometer::run, Speedometer());
+    //thread joystick_thread = thread(&Joystick::run, Joystick());
 
-    pt_thread.join();
+    //pt_thread.join();
     //cam_thread.join();
-    //radar_thread.join();
-    speedometer_thread.join();
-    joystick_thread.join();
+    radar_thread.join();
+    //speedometer_thread.join();
+    //joystick_thread.join();
 
 
     
