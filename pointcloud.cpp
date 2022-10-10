bd920b18230e038d72347ffb8da40a698ec766bb
Jinwon_PI <kjw8118@gmail.com>
2022-10-10 11:33:08 +0900

    LiDAR를 위한 PointCloud 모듈 분리

---
+++ pointcloud.cpp
@@ -0,0 +1,81 @@
+#include "pointcloud.h"
+
+
+#include <opencv2/opencv.hpp>
+#include <cmath>
+#include <vector>
+
+PointCloud::PointCloud()
+{
+    this->height = 500;
+    this->width = 500;
+    
+    this->cloud = cv::Mat::zeros(this->height, this->width, CV_64F);
+    
+}
+
+void PointCloud::putToF(ToF_t tof)
+{
+    std::vector<double> distance, angle;
+    
+    angle.assign(tof.angle.begin(), tof.angle.end());
+    distance.assign(tof.distance.begin(), tof.distance.end());
+
+    int length = tof.length;
+    cv::Mat cloud_temp = cv::Mat::zeros(this->height, this->width, CV_64F);
+    //std::cout << std::dec << (signed)length << std::endl;
+    if(length !=0)
+    {
+        for(int i=0; i<length; i++)
+        {
+            double dist = distance[i] * 0.025;
+            double ang = angle[i];
+            
+            if(dist != 0)
+            {
+                                
+                int x = (int)(dist * std::cos(ang*3.141592/180)) + this->width/2;
+                int y = (int)(dist * std::sin(ang*3.141592/180)) + this->height/2;
+                if(x < this->width && x >= 0 && y < this->height && y >= 0)
+                {
+                    //std::cout << std::dec << (signed)x << " " << (signed)y << std::endl;
+                    /*axes.at<cv::Vec3b>(y,x)[0] = 0;
+                    axes.at<cv::Vec3b>(y,x)[1] = 0;
+                    axes.at<cv::Vec3b>(y,x)[2] = 0;*/
+                    //canvas.at<cv::Vec3b>(y,x)[0] = 255;
+                    //canvas.at<cv::Vec3b>(y,x)[1] = 255;
+                    //canvas.at<cv::Vec3b>(y,x)[2] = 255;
+                    cloud_temp.at<double>(y,x) = 1;
+                }
+            }
+        }
+        for(int y=0; y<this->height; y++)
+        {
+            for(int x=0; x<this->width; x++)
+            {
+                double p = this->cloud.at<double>(y,x)*(1-this->k) + cloud_temp.at<double>(y,x)*k;
+                if(p > 1)
+                    p = 1;
+                if(p < 0)
+                    p = 0;
+                this->cloud.at<double>(y,x) = p;
+            }
+        }
+    }
+}
+
+cv::Mat PointCloud::getMap()
+{
+    cv::Mat map = cv::Mat(this->height, this->width, CV_8UC3, cv::Scalar(255, 255, 255));
+    for(int y=0; y<this->height; y++)
+    {
+        for(int x=0; x<this->width; x++)
+        {            
+            map.at<cv::Vec3b>(y,x)[0] = (uchar)((1-this->cloud.at<double>(y,x))*255);
+            map.at<cv::Vec3b>(y,x)[1] = (uchar)((1-this->cloud.at<double>(y,x))*255);
+            map.at<cv::Vec3b>(y,x)[2] = (uchar)((1-this->cloud.at<double>(y,x))*255);
+        }
+    }
+    
+    return map;
+}
---
+++ pointcloud.h
@@ -0,0 +1,36 @@
+#ifndef __POINTCLOUD_H__
+#define __POINTCLOUD_H__
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+typedef struct _ToF_t
+{        
+    std::vector<double> angle;
+    std::vector<double> distance;
+    int length;
+} ToF_t;
+
+class PointCloud
+{
+private:
+    cv::Mat cloud;
+    int height;
+    int width;
+    double k = 0.2;
+public:
+    PointCloud();
+    //~PointCloud();
+    void putToF(ToF_t tof);
+    cv::Mat getMap();
+    //int getMap();
+};
+
+
+
+
+
+
+
+
+#endif
