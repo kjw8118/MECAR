e4b752fc72e77c8aa5a6739eeff0a15434bc46d6
Jinwon_PI <kjw8118@gmail.com>
2022-10-12 00:24:10 +0900

    HMC5993L 구현 완료

--- hmc5883l.cpp
+++ hmc5883l.cpp
@@ -9,79 +9,111 @@
 
 #include <opencv2/opencv.hpp>
 
-void HMC5883L::init()
+HMC5883L_Data::HMC5883L_Data(const HMC5883L_Data& origin)
 {
-    this->i2c.begin(this->ADR);
+    this->mx = origin.mx;
+    this->my = origin.my;
+    this->mz = origin.mz;
+}
+
+HMC5883L::HMC5883L(uint8_t address, uint8_t WHO_RET)
+:   address(address), WHO_RET(WHO_RET)
+{
+
+}
+
+HMC5883L::HMC5883L(uint8_t address)
+:   address(address)
+{
+    this->WHO_RET = 0;
+}
+HMC5883L::~HMC5883L()
+{
+
+}
+int HMC5883L::init()
+{
+    this->i2c.begin(this->address);
     usleep(50000);
-    this->i2c.writeReg(0x00, 0x70); usleep(100000);
-    this->i2c.writeReg(0x01, 0xA0); usleep(100000);
-    this->i2c.writeReg(0x02, 0x00); usleep(100000);
-    //std::cout << std::hex << "CFG_A " << (unsigned)this->i2c.readReg(this->CFG_A) << std::endl;
-    //std::cout << std::hex << "CFG_B " << (unsigned)this->i2c.readReg(this->CFG_B) << std::endl;
-    //std::cout << std::hex << "MODE " << (unsigned)this->i2c.readReg(this->MOD) << std::endl;
-    /*
-    //uint8_t ida = this->i2c.readReg(this->IDA); usleep(50000);
-    //uint8_t idb = this->i2c.readReg(this->IDB); usleep(50000);
-    //uint8_t idc = this->i2c.readReg(this->IDC); usleep(50000);
-    //std::cout << (unsigned)ida << "_" << (unsigned)idb << "_" << (unsigned)idc << std::endl;
 
-    //uint8_t cfg_reg_a_buf[3] = {this->WRT, this->CFG_A, 0};
-    uint8_t cfg_reg_a = 0;
-    cfg_reg_a |= 0<<5; // Sample movemean
-    cfg_reg_a |= 4<<2; // 15Hz (Default)
-    cfg_reg_a |= 0; // Measurement mode default
-    //cfg_reg_a_buf[2] = cfg_reg_a;
-    //this->i2c.writeByte(this->WRT); usleep(10000);    
-    this->i2c.writeReg(this->CFG_A, cfg_reg_a); usleep(100000);    
-    //std::cout << std::hex << "CFG_A " << (unsigned)cfg_reg_a << " -> " << (unsigned)this->i2c.readReg(this->CFG_A) << std::endl;
-    //this->i2c.writeBytes(cfg_reg_a_buf, 3); usleep(50000);
-
-    //uint8_t cfg_reg_b_buf[3] = {this->WRT, this->CFG_B, 0};
-    uint8_t cfg_reg_b = 0;
-    cfg_reg_b |= 1<<5; // Gain 1.3Ga (Default) -> factor for 1090
-    //cfg_reg_b_buf[2] = cfg_reg_b;
-    //this->i2c.writeByte(this->WRT); usleep(10000);
-    this->i2c.writeReg(this->CFG_A, cfg_reg_b); usleep(100000);
-    //std::cout << std::hex << "CFG_B " << (unsigned)cfg_reg_b << " -> " << (unsigned)this->i2c.readReg(this->CFG_B) << std::endl;    
-    //this->i2c.writeBytes(cfg_reg_b_buf, 3); usleep(50000);
-
-    //uint8_t mode_reg_buf[3] = {this->WRT, this->MOD, 0};
-    uint8_t mode_reg = 0; // Operating Mode Continuous-mode
-    //mode_reg_buf[2] = mode_reg;
-    //this->i2c.writeByte(this->WRT); usleep(10000);
-    this->i2c.writeReg(this->MOD, mode_reg); usleep(100000);
-    //std::cout << std::hex << "MOD " << (unsigned)mode_reg << " -> " << (unsigned)this->i2c.readReg(this->MOD) << std::endl;
-    //this->i2c.writeBytes(mode_reg_buf, 3); usleep(50000);
-    */
+    uint8_t who_ret = this->i2c.readReg(this->IDA);
     
+    this->i2c.writeReg(this->CFG_A, 0x70); usleep(100000);
+    this->i2c.writeReg(this->CFG_B, 0xA0); usleep(100000);
+    this->i2c.writeReg(this->MOD, 0x00); usleep(100000);
+
+    if(who_ret != this->WHO_RET)
+    {
+        std::cout << "HMC5883L self test: " << std::hex << (unsigned)who_ret << std::endl;
+        
+        for(int i=0; i<13; i++)
+        {
+            std::cout << std::hex << i << " -> " << (unsigned)this->i2c.readReg(i) << std::endl;
+        }
+        return -1;
+    }
+    std::cout << "---------------------- HMC5883L init ----------------------" << std::endl;
+    std::cout << std::hex << "CFG_A: " << (unsigned)this->i2c.readReg(this->CFG_A) << std::endl;
+    std::cout << std::hex << "CFG_B: " << (unsigned)this->i2c.readReg(this->CFG_B) << std::endl;
+    std::cout << std::hex << "MODE: " << (unsigned)this->i2c.readReg(this->MOD) << std::endl;
+    std::cout << std::hex << "STATUS: " << (unsigned)this->i2c.readReg(this->STA) << std::endl;
+    std::cout << std::hex << "ID A: " << (unsigned)this->i2c.readReg(this->IDA) << std::endl;
+    std::cout << std::hex << "ID B: " << (unsigned)this->i2c.readReg(this->IDB) << std::endl;
+    std::cout << std::hex << "ID C: " << (unsigned)this->i2c.readReg(this->IDC) << std::endl;
+    /*for(int i=0; i<3; i++)
+    {
+        uint8_t msb = this->i2c.readReg(i*2+3);
+        uint8_t lsb = this->i2c.readReg(i*2+4);
+        std::cout << std::hex << i << " -> " << (uint16_t)(msb<<8 | lsb) << std::endl;
+    }*/
+    std::cout << "-----------------------------------------------------------" << std::endl;
+    return 0;
+}
+
+bool HMC5883L::getStatus()
+{
+    uint8_t status = this->i2c.readReg(this->STA);
+    if(status & 1)            
+        return true;    
+    else            
+        return false;        
 }
 void HMC5883L::update()
 {
     uint8_t buf[6] = {0,};
-    //this->i2c.writeByte(this->RED);
-    //uint8_t cmd[2] = {0x0d, 0x06};
-    //this->i2c.writeBytes(cmd, 2); usleep(100000);
-
+    
     this->i2c.readReg(this->XOUT_H, buf, 6);
-    this->compass[0] = (double)((int16_t)(buf[0]<<8 | buf[1]))/1090;
-    this->compass[1] = (double)((int16_t)(buf[2]<<8 | buf[3]))/1090;
-    this->compass[2] = (double)((int16_t)(buf[4]<<8 | buf[5]))/1090;
-    //std::cout << (int16_t)(buf[0]<<8 | buf[1]) << "\t" << (int16_t)(buf[2]<<8 | buf[3]) << "\t" << (int16_t)(buf[4]<<8 | buf[5]) << std::endl;
-    //this->i2c.writeByte(this->WRT);
-    //this->i2c.writeByte(this->XOUT_H);
+
+    //std::cout << std::hex << (unsigned)((buf[0]&0xff)<<8 | (buf[1]&0xff)) << "\t" << (unsigned)((buf[2]&0xff)<<8 | (buf[3]&0xff)) << "\t" << (unsigned)((buf[4]&0xff)<<8 | (buf[5]&0xff)) << std::endl;
+
+
+    this->data.mx = (double)((int16_t)(buf[0]<<8 | buf[1]))/230;
+    this->data.my = (double)((int16_t)(buf[2]<<8 | buf[3]))/230;
+    this->data.mz = (double)((int16_t)(buf[4]<<8 | buf[5]))/230;
+    
 }
 void HMC5883L::run()
 {
     this->init();
     std::cout.precision(3);
 
-    while(true)
+    while(this->getStatus())
     {
         this->update();
 
-        std::cout << std::fixed << this->compass[0] << "\t" << this->compass[1] << "\t" << this->compass[2] << std::endl;
+        //std::cout << std::fixed << this->data.mx << "\t" << this->data.my << "\t" << this->data.mz << std::endl;
 
         usleep(10000);
         
     }
+
+    std::cout << "HMC5883L exit" << std::endl;
+}
+
+void HMC5883L::getData(cv::Vec3d& magnet)
+{
+    magnet[0] = this->data.mx;
+    magnet[1] = this->data.my;
+    magnet[2] = this->data.mz;
+    
 }
--- hmc5883l.h
+++ hmc5883l.h
@@ -5,12 +5,27 @@
 
 #include <opencv2/opencv.hpp>
 
+
+
+class HMC5883L_Data
+{
+public:    
+    double mx = 0;
+    double my = 0;
+    double mz = 0;
+    HMC5883L_Data(){};
+    ~HMC5883L_Data(){};
+    HMC5883L_Data(const HMC5883L_Data& origin);
+};
+
 class HMC5883L
 {
 private:
+    uint8_t address = 0;
+    uint8_t WHO_RET = 0;
     enum REGISTER
     {
-        ADR = 0x1e,
+        //ADR = 0x1e,
         WRT = 0x3c,
         RED = 0x3d,
         CFG_A = 0x00,
@@ -23,22 +38,25 @@ private:
         ZOUT_H = 0x07,
         ZOUT_L = 0x08,        
         STA = 0x09,
-        IDA = 0x10,
-        IDB = 0x11,
-        IDC = 0x12,
+        IDA = 0x0a,
+        IDB = 0x0b,
+        IDC = 0x0c,
         
 
     };
     
     GPIO::Wire i2c;
-
-    cv::Vec3d compass;    
+    HMC5883L_Data data;    
+        
 
 public:
-    HMC5883L(/* args */) {};
-    ~HMC5883L() {};
-    void init();
+    HMC5883L(uint8_t address, uint8_t WHO_RET);
+    HMC5883L(uint8_t address);
+    ~HMC5883L();
+    int init();
+    bool getStatus();
     void update();
+    void getData(cv::Vec3d& magnet);
     void run();
 
 };
